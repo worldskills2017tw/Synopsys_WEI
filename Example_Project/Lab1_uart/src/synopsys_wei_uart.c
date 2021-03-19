da7709e2c19c29450a7bbbd86f6d93b598841155
worldskills2017tw <62198334+worldskills2017tw@users.noreply.github.com>
2021-03-19 14:43:22 +0800

    Add files via upload

---
+++ Example_Project/Lab1_uart/src/synopsys_wei_uart.c
@@ -0,0 +1,20 @@
+#include "synopsys_wei_uart.h"
+
+
+
+volatile void hal_test(uint8_t * char_data)
+{
+  *char_data = 0x12;
+}
+
+HAL_RETURN hal_uart_get_char(uint8_t * char_data)
+{
+  uint8_t data_buf = 0;
+  if(hx_drv_uart_getchar(&data_buf) != HX_DRV_LIB_PASS)
+    return HAL_ERROR;
+  else 
+  {
+    *char_data = data_buf;
+    return HAL_OK;
+  }
+}
